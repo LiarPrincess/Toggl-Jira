675d5cdb1de17f0b02a4d324d483fa82baf4e39a
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2018-07-03 20:22:53 +0200

    [Tests] Added Jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "globals": {
+    "ts-jest": {
+      "tsConfigFile": "tsconfig.json"
+    }
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+  ],
+}
---
+++ src/util/__tests__/pretty-print.spec.ts
@@ -0,0 +1,9 @@
+import moment from "moment";
+import * as pretty from "./../pretty-print";
+
+test("test", () => {
+  const x = 2;
+  const a = 5;
+  pretty.moment(moment());
+  expect(0).toBe(0);
+});
